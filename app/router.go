18fafcf7f435fab149599228614a175570eac057
Hamstag <hamstagroup@gmail.com>
2024-07-06 16:21:20 +0700

    add middleware to route list

--- app/router.go
+++ app/router.go
@@ -20,16 +20,7 @@ var initRoutes []func(app *App)
 func (app *App) initRouter() {
 	app.router = chi.NewRouter()
 
-	app.router.Use(
-		render.SetContentType(render.ContentTypeJSON),
-		cors.AllowAll().Handler,
-		middleware.RequestID,
-		middleware.RealIP,
-		middleware.Logger,
-		middleware.Compress(5),
-		middleware.Heartbeat("/ping"),
-		middleware.Recoverer,
-	)
+	app.router.Use(baseMiddleware)
 
 	app.apiRouter = chi.NewMux()
 	app.router.Mount(app.cfg.APIPrefix, app.apiRouter)
@@ -44,18 +35,40 @@ func OnInitRoutes(fn func(app *App)) {
 	initRoutes = append(initRoutes, fn)
 }
 
+func baseMiddleware(next http.Handler) http.Handler {
+	middlewares := []func(http.Handler) http.Handler{
+		render.SetContentType(render.ContentTypeJSON),
+		cors.AllowAll().Handler,
+		middleware.RequestID,
+		middleware.RealIP,
+		middleware.Logger,
+		middleware.Compress(5),
+		middleware.Heartbeat("/ping"),
+		middleware.Recoverer,
+	}
+
+	return chi.Chain(middlewares...).Handler(next)
+}
+
 func (app *App) routeList() error {
+	replacer := strings.NewReplacer("go-minimalists/", "")
+
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
 	fmt.Fprintf(w, "%s\t%s\t%s\n", "Method", "Route", "Handler")
 	fmt.Fprintf(w, "%s\t%s\t%s\n", "------", "-----", "-------")
 
-	walkFunc := func(method string, route string, handler http.Handler, middleware ...func(http.Handler) http.Handler) error {
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
 		route = strings.Replace(route, "/*/", "/", -1)
 		pathHandler := runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
-		replacer := strings.NewReplacer("go-minimalists", "")
 		pathHandler = replacer.Replace(pathHandler)
 
 		fmt.Fprintf(w, "%s\t%s\t%s\n", method, route, pathHandler)
+
+		for _, mw := range middlewares {
+			pathMiddleware := runtime.FuncForPC(reflect.ValueOf(mw).Pointer()).Name()
+			pathMiddleware = replacer.Replace(pathMiddleware)
+			fmt.Fprintf(w, "\t\t⇂ %s\n", pathMiddleware)
+		}
 
 		return nil
 	}
